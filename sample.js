22d4038c4bd646231373468ec5a212ab01474ec2
小林大輝 <kobayashidaiki@daikiPC.local>
2022-01-26 12:53:24 +0900

    [Finish]1-3.1

--- sample.js
+++ sample.js
@@ -57,3 +57,72 @@
 // }
 // foo()
 // 変数の巻き上げが起こっている。関数内のどの部分で変数を宣言しても、関数の冒頭部分で変数の宣言をしていることになる。
+
+
+// ----1-3
+
+// let alertStrig;
+// alertString = addString("WebCamp");
+
+// alert(alertString)
+
+// function addString(strA) {
+//   let addStr = "Hello " + strA;
+//   return addStr;
+// }
+
+// let promptStr = prompt('何か好きな文字を入力してください');
+// alert(promptStr);
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand,js_hand);
+
+alert('あなたの選んだては' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+function getJShand() {
+  let js_hand_num = Math.floor(Math.random()*3);
+
+  if(js_hand_num == 0) {
+    hand_hand = "グー";
+  } else if(js_hand_num == 1) {
+    hand_hand = "チョキ";
+  } else if(js_hand_num == 2) {
+    hand_hand = "パー";
+  }
+  return hand_hand;
+}
+
+function winLose(user,js) {
+  let winLoseStr;
+
+  if(user == "グー") {
+    if(js == "グー") {
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ") {
+      winLoseStr = "勝ち";
+    } else if(js == "パー") {
+      winLoseStr = "負け"
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
